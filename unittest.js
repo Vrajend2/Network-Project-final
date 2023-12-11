702cc6f712e2a70c9d2810de3379b26a93bd01a5
Vrajend2 <143967988+Vrajend2@users.noreply.github.com>
2023-12-11 16:58:34 -0500

    commit now

--- unittest.js
+++ unittest.js
@@ -3,11 +3,11 @@ import {calculatevalue}  from "../src/utils"
 
 test('calculates the value correctly', () => {
     
-    const allocatedamount= 250;
+    const allocatedamount= 370;
     const totalBudgetamount = 5000;
    
     
      const result = calculatevalue(allocatedamount, totalBudgetamount);
    
-    expect(result).toBe('5.00');
+    expect(result).toBe('7.4');
    });
